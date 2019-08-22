11d204abb595a387066613840c05b9b409800bc5
yuktish <syukti976@gmail.com>
2019-08-22 11:20:08 +0530

    single page applications part1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,7 +5,11 @@ import DishDetail from './DishDetailComponent';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
 
@@ -13,7 +17,9 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
-        selectedDish: null
+        comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -24,7 +30,9 @@ class Main extends Component {
   render() {
     const HomePage = () => {
         return(
-            <Home 
+            <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+            promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+            leader={this.state.leaders.filter((leader) => leader.featured)[0]} 
             />
         );
       }
@@ -34,6 +42,7 @@ class Main extends Component {
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path="/contactus" component={Contact} />
               <Redirect to="/home" />
           </Switch>
         
